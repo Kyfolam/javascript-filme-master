d5c607e0c57808e4311af7951c8312c8ff0f845d
Kyfolam <Michael.kydd@gmx.de>
2020-06-04 13:12:41 +0200

    Initial commit

---
+++ dao.js
@@ -0,0 +1,19 @@
+export class DAO{
+    // Michael Kydd
+
+    read(){
+        let dataArray;
+        if(localStorage.getItem('movies')){
+            let dataString = localStorage.getItem('movies');
+            dataArray = JSON.parse(dataString);
+        }else{
+            dataArray = [];
+        }
+        return dataArray;
+    }
+
+    write(dataArray){
+        localStorage.setItem('movies', JSON.stringify(dataArray));
+    }
+
+}
---
+++ view.js
@@ -0,0 +1,90 @@
+export class View {
+    // Michael Kydd
+    constructor(presenter) {
+        // CONTROLS
+        this.inputTitel = document.getElementById('inputTitel');
+        this.inputRegie = document.getElementById('inputRegie');
+
+        this.sliderBewertung = document.getElementById('sliderBewertung');
+        this.sterneBewertung = document.getElementById('sterneBewertung');
+
+        this.radioBergfilm = document.getElementById('radioBergfilm');
+        this.radioRoadmovie = document.getElementById('radioRoadmovie');
+        this.radioWestern = document.getElementById('radioWestern');
+
+        this.buttonNeu = document.getElementById('buttonNeu');
+
+        this.tdBergfilm = document.getElementById('tdBergfilm');
+        this.tdRoadmovie = document.getElementById('tdRoadmovie');
+        this.tdWestern = document.getElementById('tdWestern');
+        this.tdGesamt = document.getElementById('tdGesamt');
+
+        this.ulNeueste = document.getElementById('ulNeueste');
+        this.ulAlle = document.getElementById('ulAlle');
+
+        // EVENTS
+        this.buttonNeu.addEventListener('click', function () {
+            presenter.buttonNeuClick();
+        });
+
+        this.sliderBewertung.addEventListener('change', () => {    
+            presenter.sliderBewertungChange();
+        })
+    }
+
+    // GETTER
+    getTitel() {
+        return this.inputTitel.value;
+    }
+
+    getRegie(){
+        return this.inputRegie.value;
+    }
+
+    getBewertung() {
+        return this.sliderBewertung.value;
+    }
+    isSelectedBergfilm() {
+        return this.radioBergfilm.checked;
+    }
+    isSelectedRoadmovie() {
+        return this.radioRoadmovie.checked;
+    }
+    isSelctedWestern(){
+        return this.radioWestern.checked;
+    }
+
+    // SETTER
+    displayAnzahlBergfilm(zahl) {
+        this.tdBergfilm.innerHTML = zahl;
+    }
+    displayAnzahlRoadmovie(zahl) {
+        this.tdRoadmovie.innerHTML = zahl;
+    }
+    displayAnzahlWestern(zahl){
+        this.tdWestern.innerHTML = zahl;
+    }
+    displayAnzahlGesamt(zahl) {
+        this.tdGesamt.innerHTML = zahl;
+    }
+    displayLast(stringArray) {
+        this._displayList(this.ulNeueste, stringArray);
+    }
+    displayAll(stringArray) {
+        this._displayList(this.ulAlle, stringArray);
+    }
+    displaySterneBewertung(string){
+        this.sterneBewertung.innerHTML = string;
+    }
+
+    // PRIVATE
+    _displayList(ul, stringArray) {
+        ul.innerHTML = '';
+        stringArray.forEach(element => {
+            const liNode = document.createElement('li');
+            ul.appendChild(liNode);
+            const textNode = document.createTextNode(element);
+            liNode.appendChild(textNode);
+        });
+    }
+}
